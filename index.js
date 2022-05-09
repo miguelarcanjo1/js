a804b3d20ac3d4937d7c7bb769114110b0a152b8
miguelarcanjo1 <102737532+miguelarcanjo1@users.noreply.github.com>
2022-05-09 08:10:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+//Exercicios- Miguel Arcanjo
+//1-a
+console.log(10+20*30)
+
+//1-b
+console.log(4**2/30)
+
+//1-c
+
+console.log((9**4+2)*6-1)
+
+//2-A
+console.log(10 % 3 * 10** 1 + 1 - 10 * 4 / 2)
+
+//3
+
+console.log('Miguel Arcanjo')
+
+//4
+
+console.log((2*3)*(3*5))
+
+//5
+
+console.log(a+b+c)
+
+//6
+
+const salario = 1800
+console.log(salario*2)
